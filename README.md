58a34fcd29ded2ac99dac1662d832c760833d0cb
Omprakaxh <omprakaxh@gmail.com>
2026-04-15 09:14:25 +0530

    updated the py file for reading txt file
    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # My Project
+
+This project is an practice project for my DE journey, 
+
+## Day 1 : 
+
+Reading files from csv, json and txt.
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Sample project')
+# Reading file from an txt.
+
+print("\n The Method 1")
+
+# Method 1 reading the whole file
+with open("story.txt", "r") as file:
+    content = file.read()
+
+print(content)
+
+print("\n The Method 2")
+
+# Method 2 reading line by line
+with open("story.txt","r") as file:
+    for line in file:
+        print(line.strip())
+
+
+
+
