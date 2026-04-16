3b279a6c992ba7b3518f5154616b2d2209a5f58d
Omprakaxh <omprakaxh@gmail.com>
2026-04-16 08:39:22 +0530

    mini challenge for r/w
    Day 2

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ This project is an practice project for my DE journey,
 ## Day 1 : 
 
 Reading files from csv, json and txt.
+
+### story.txt
+
+Alice went to the market.
+She bought apples, bananas, and grapes.
+On her way back, she met Bob.
+Bob asked, "What did you buy?"
+Alice smiled and said, "Fruits for the week!"
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""
+
 # Reading file from an txt.
 
 print("\n The Method 1")
@@ -15,6 +17,59 @@ with open("story.txt","r") as file:
     for line in file:
         print(line.strip())
 
+# Method 3 read all lines into a list
+
+print("\n Method 3")
+
+with open("story.txt","r") as file:
+    lines = file.readlines()
+print(lines) 
 
 
+
+# Mini challenge - 1
+
+with open("story.txt","r") as file:
+    lines =  file.readlines()
+
+for line in lines:
+    if "Alice" in line:
+        print(line.strip())
+
+
+# Mini challenge 2
+
+with open("story.txt","r") as f:
+    lines = f.readlines()
+index = 1
+for line in lines:
+    print(f"{index}. {line.strip()}")
+    index+=1
+
+
+
+# Mini challenge 3 - count total words
+
+with open("story.txt","r") as f:
+    lines = f.readlines()
+count = 0
+for line in lines:
+    word = line.split()
+    count += len(word)
+print(count)
+
+"""
+
+# Mini challenge 4  - write a python file
+
+with open("story.txt","r") as f:
+    lines = f.readlines()
+
+with open("output.txt","w") as out:
+    for line in lines:
+        word = line.split()
+        if len(word) > 5:
+            out.write(line)
+
+        
 
